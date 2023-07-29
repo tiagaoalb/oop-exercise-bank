b1cb605838608f29c8ed930661fd2a732ab27f06
Tiago <tiagaoalb@gmail.com>
2023-07-29 19:46:21 -0300

    chore: add methods

--- src/entities/Account.java
+++ src/entities/Account.java
@@ -2,30 +2,38 @@ package entities;
 
 public class Account {
 
-    protected int accountNumber;
-    private String name;
-    private double depositValue;
+    protected int number;
+    private String holder;
+    private double balance;
 
     public Account() {
     }
 
-    public Account(int accountNumber, String name, double depositValue) {
-        this.accountNumber = accountNumber;
-        this.name = name;
-        this.depositValue = depositValue;
+    public Account(int number, String holder, double balance) {
+        this.number = number;
+        this.holder = holder;
+        this.balance = balance;
     }
 
-    public Account(int accountNumber, String name) {
-        this.accountNumber = accountNumber;
-        this.name = name;
-        depositValue = 0;
+    public Account(int number, String holder) {
+        this.number = number;
+        this.holder = holder;
+        balance = 0;
     }
 
     public String getName() {
-        return name;
+        return holder;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setName(String holder) {
+        this.holder = holder;
+    }
+
+    public void deposit(double amount) {
+        this.balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        this.balance -= amount;
     }
 }
