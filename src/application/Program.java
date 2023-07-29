bcdc1edf35e01170c8490c157ef9bac8c67b0787
Tiago <tiagaoalb@gmail.com>
2023-07-28 23:55:54 -0300

    chore: checkpoint

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,4 +1,11 @@
 package application;
 
+import java.util.Scanner;
+
 public class Program {
+  public static void main (String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    sc.close();
+  }
 }
