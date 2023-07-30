04d21adf96f4a92e38fadcddfd6c94da3d2e1fb6
Tiago <tiagaoalb@gmail.com>
2023-07-30 12:01:30 -0300

    chore: add methods

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Program {
   public static void main (String[] args) {
     Scanner sc = new Scanner(System.in);
+    System.out.println("Test");
 
     sc.close();
   }
